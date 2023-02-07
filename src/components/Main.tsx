4a7b9c7cd7c840fa6efb4b4b905bc70f6ade8a69
kopcsak patirk <kopcsakpatrik1@gmail.com>
2023-02-07 08:31:56 +0100

    main, footer

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import { Component, ReactNode } from "react";
+
+
+export default class Main extends Component {
+    render(): ReactNode {
+        return <main>
+            <p>This is Zsigmond homosexual </p>
+        </main>
+    }
+}
