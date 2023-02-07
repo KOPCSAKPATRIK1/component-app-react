164ff55cccdadae66049a675a9018527bde6b67b
kopcsak patirk <kopcsakpatrik1@gmail.com>
2023-02-07 08:32:11 +0100

    feet, main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default class App extends Component {
+  render(): React.ReactNode {
+    return <div>
+      <Main></Main>
+      <Main></Main>
+      <Footer></Footer>
     </div>
-  );
-}
-
-export default App;
+  }
+};
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import { Component, ReactNode } from "react";
+
+
+export default class Footer extends Component {
+    render(): ReactNode {
+        return <footer>
+            <p>This is Footer Zsigmond feetsexual </p>
+        </footer>
+    }
+}
